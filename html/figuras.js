02d9792901036c0bfaeed5d2727c71430a211485
Chauxxx06 <freemanchx@hotmail.com>
2022-04-16 18:19:46 -0700

    Creando funcion en Javascripts

--- html/figuras.js
+++ html/figuras.js
@@ -2,31 +2,45 @@
 //el Cuadrado
 
 console.group("Cuadrado")
-const ladoCuadrado = 5;
-console.log("los cuadrados del cuadrado miden: " + ladoCuadrado + " cm");
-const perimetroCuadrado = 5*4;
-console.log("El perimetro del cuadrado es: " + perimetroCuadrado + " cm");
-const areaCuadrado = Math.pow(ladoCuadrado,2);
-console.log("El area del cuadrado es: " + areaCuadrado + " cm2");
+//const ladoCuadrado = 5;
+//(console.log("los cuadrados del cuadrado miden: " + ladoCuadrado + " cm");
+function perimetroCuadrado(lado){
+    return lado*4;
+}
+console.log("El perimetro del cuadrado es: " + perimetroCuadrado(10) + " cm");
+function areaCuadrado(lado) {
+    return Math.pow(lado,2);
+}
+console.log("El area del cuadrado es: " + areaCuadrado(10) + " cm2");
 console.groupEnd();
 
 //El triagulo (isoceles)
 console.group("Triangulo")
-const ladoTriangulo = 6;
-const baseTriangulo = 4;
-let perimetroTriangulo = ladoTriangulo*2 + baseTriangulo;
-let alturaTriangulo = Math.sqrt(Math.pow(ladoTriangulo,2) - Math.pow(baseTriangulo/2,2));
-let areaTriangulo = ((alturaTriangulo*baseTriangulo)/2)
-console.log("El perimetro del triangulo es " + perimetroTriangulo + " cm");
-console.log("La altura del triangulo es " + alturaTriangulo + " cm");
-console.log("El area del triangulo es " + areaTriangulo + " cm2");
+/* const ladoTriangulo = 6;
+const baseTriangulo = 4; */
+function perimetroTriangulo(lado, base) {
+    return lado*2 + base;
+}
+function alturaTriangulo(lado, base) {
+    return Math.sqrt(Math.pow(lado,2) - Math.pow(base/2,2));
+}
+function areaTriangulo(altura, base){
+    return ((altura*base)/2)
+}
+console.log("El perimetro del triangulo es " + perimetroTriangulo(6,4) + " cm");
+console.log("La altura del triangulo es " + alturaTriangulo(6,4) + " cm");
+console.log("El area del triangulo es " + areaTriangulo(alturaTriangulo(6,4),4) + " cm2");
 console.groupEnd();
 
 //El Circulo 
 console.group("Circulo")
-const radius = 4;
-let perimetroCirculo = 2*Math.PI*radius;
-let areaCirculo = Math.PI*Math.pow(radius,2);
-console.log("El perimetro del circulo es: " + perimetroCirculo + " cm");
-console.log("El area del circulo es: " + areaCirculo + " cm2");
+//const radius = 4;
+function perimetroCirculo(radius) {
+    return 2*Math.PI*radius;
+}
+function areaCirculo(radius){
+    return Math.PI*Math.pow(radius,2);
+}
+console.log("El perimetro del circulo es: " + perimetroCirculo(4) + " cm");
+console.log("El area del circulo es: " + areaCirculo(4) + " cm2");
 console.groupEnd();
